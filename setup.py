b0dbf99643a93313f82f7100eebe65f7e2c75c44
Swen Kooij <swen@sectorlabs.ro>
2025-06-10 14:00:23 +0200

    Add support for Jinja3

--- setup.py
+++ setup.py
@@ -39,7 +39,8 @@ sdict = {
     'packages' : ['jinjasql'],
     'test_suite' : 'tests.all_tests',
     'install_requires': [
-        'Jinja2>=2.5,<3.0'
+        'Jinja2>=2.5,<4.0',
+        'markupsafe>=2.0,<=4',
     ],
     'classifiers' : [
         'Development Status :: 4 - Beta',
